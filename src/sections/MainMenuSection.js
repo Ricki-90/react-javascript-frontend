4a205a72dcb246eef278824767cfd4c0aff83b64
Rickard <rickard.johansson@ecutbildning.se>
2022-10-18 21:16:54 +0200

    added scss

--- src/sections/MainMenuSection.js
+++ src/sections/MainMenuSection.js
@@ -1,24 +1,25 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 import MenuIcon from '../components/MenuIcon'
 
 function MainMenuSection() {
   return (
-    <nav class="mainmenu container">
-        <div class="logo">
-            Fixxo.
-        </div>
-        <div class="menu-links">
-            <a href="index.html">Home</a>
-            <a href="#">Categories</a>
-            <a href="#">Products</a>
-            <a href="contacts.html">Contacts</a>
-        </div>
-        <div class="menu-icons">
-            <MenuIcon link="/search" icon="fa-regular fa-magnifying-glass" />
-            <MenuIcon link="/compare" icon="fa-light fa-code-compare" />
-            <MenuIcon link="/wishlist" icon="fa-light fa-heart" />
-            <MenuIcon link="/shoppingcart" icon="fa-light fa-bag-shopping" />
-        </div>
+    <nav className="mainmenu container">
+      <div className="logo">
+          Fixxo.
+      </div>
+      <div className="menu-links">
+          <NavLink className="menu-link" to="/">Home</NavLink>
+          <NavLink className="menu-link" to="/categories">Categories</NavLink>
+          <NavLink className="menu-link" to="/products">Products</NavLink>
+          <NavLink className="menu-link" to="/contacts">Contacts</NavLink>
+      </div>
+      <div className="menu-icons">
+          <MenuIcon link="/search" icon="fa-regular fa-magnifying-glass" />
+          <MenuIcon link="/compare" icon="fa-light fa-code-compare" />
+          <MenuIcon link="/wishlist" icon="fa-light fa-heart" />
+          <MenuIcon link="/shoppingcart" icon="fa-light fa-bag-shopping" />
+      </div>
     </nav>
   )
 }
