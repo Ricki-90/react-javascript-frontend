5b9d798f01e6d9fa2e71071c2290d36e2efc12d8
Rickard <rickard.johansson@ecutbildning.se>
2022-10-21 10:10:11 +0200

    Added scss for mobileFirst

--- src/components/MenuIcon.js
+++ src/components/MenuIcon.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 
-function MenuIcon({link, icon, quantity, hideMobile}) {
+function MenuIcon({link, icon, quantity, hideOnMobile, hideOnTablet}) {
+
   return (
-    <NavLink className="menu-icon" style={(hideMobile) => hideMobile ? {}} to={link} end>
+    <NavLink className={`menu-icon ${hideOnMobile ? "hidden" : ""} ${hideOnTablet ? "hidden" : ""}`} to={link} end>
       <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-warning">{quantity}</span>
       <i className={icon}></i>
     </NavLink>
