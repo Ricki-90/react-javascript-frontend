d366d4f7147f863a3fb02d5a4d57671b2d1b3eb8
Rickard <rickard.johansson@ecutbildning.se>
2022-10-21 13:23:12 +0200

    added hideOnMobil effect on menuicon

--- src/sections/MainMenuSection.js
+++ src/sections/MainMenuSection.js
@@ -3,11 +3,12 @@ import { NavLink } from 'react-router-dom'
 import MenuIcon from '../components/MenuIcon'
 
 function MainMenuSection() {
+
+
+  
   return (
     <nav className="mainmenu container">
-      <div className="logo">
-          <NavLink className="logo" to="/" end>Fixxo.</NavLink>
-      </div>
+        <NavLink className="logo" to="/" end>Fixxo.</NavLink>
       <div className="menu-links">
           <NavLink className="menu-link" to="/" end>Home</NavLink>
           <NavLink className="menu-link" to="/categories" end>Categories</NavLink>
@@ -16,9 +17,12 @@ function MainMenuSection() {
       </div>
       <div className="menu-icons">
           <MenuIcon link="/search" icon="fa-regular fa-magnifying-glass" />
-          <MenuIcon hideOnMobile="true" link="/compare" icon="fa-light fa-code-compare" />
-          <MenuIcon hideOnMobile="true" quantity="3" link="/wishlist" icon="fa-light fa-heart" />
+          <MenuIcon hideOnMobile={true} link="/compare" icon="fa-light fa-code-compare" />
+          <MenuIcon hideOnMobile={true} quantity="3" link="/wishlist" icon="fa-light fa-heart" />
           <MenuIcon quantity="4" link="/shoppingcart" icon="fa-light fa-bag-shopping" />
+
+          <button className="d-xl-none menu-icon btn-menu-icon"><i className="fa-regular fa-bars"></i></button>
+
       </div>
     </nav>
   )
