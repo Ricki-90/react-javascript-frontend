5acd6499e52d22424fa3314ef56aeb5fceed57c0
Rickard <rickard.johansson@ecutbildning.se>
2022-10-25 19:48:21 +0200

    Added submitted on contactform

--- src/sections/ContactFormSection.js
+++ src/sections/ContactFormSection.js
@@ -3,18 +3,30 @@ import React, {useState} from 'react'
 const ContactFormSection = () => {
     const [contactForm, setContactForm] = useState ({name: '', email: '', comment: ''})
     const [formErrors, setFormErrors] = useState({})
+    const [submitted, setSubmitted] = useState(false)
 
     const validate = (values) => {
       const errors = {}
+      const regex_email = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
       if(!values.name)
         errors.name = "You must enter a name"
 
       if(!values.email)
-      errors.email = "You must enter an e-mail address"
+        errors.email = "You must enter an e-mail address"
+      else if (!regex_email.test(values.email))
+        errors.email = "You must enter a valid e-mail address (eg. name@domain.com)"
 
       if(!values.comment)
-      errors.name = "You must enter a comment"
+        errors.comment = "You must enter a comment"
+      else if(values.comment.length < 5)
+        errors.comment = "Your comment must be longer then five characters"
+
+      if(Object.keys(errors).length === 0)
+        setSubmitted(true)
+      else
+        setSubmitted(false)
+
 
       return errors;
     }
@@ -36,25 +48,36 @@ const ContactFormSection = () => {
   return (
     <section className="contact-form">
         <div className="container">
-            <h2>Come in Contact with Us</h2>
-            <pre></pre>
-            <form onSubmit={handleSubmit} noValidate>
-                <div>
-                    <input id="name" typ="text" placeholder="Your Name" value={contactForm.name} onChange={handleChange} />
-                    <div className="errorMessage">{formErrors.name}</div>
-                </div>
-                <div>
-                    <input id="email" typ="email" placeholder="Your Mail" value={contactForm.email} onChange={handleChange} />
-                    <div className="errorMessage">{formErrors.email}</div>
-                </div>
-                <div className="textarea">
-                    <textarea id="comment" placeholder="Comments" value={contactForm.comment} onChange={handleChange}></textarea>
-                    <div className="errorMessage">{formErrors.comment}</div>
-                </div>
-                <div className="formBtn">
-                  <button type="submit" className="btn-theme">Post Comments</button>
-                </div>
-            </form>
+          {
+            submitted ?
+            <div className="d-flex justify-content-center align-items-center">
+              <div>Thank you for your comment!</div>
+            </div>
+            :
+            (
+              <>
+              <h2>Come in Contact with Us</h2>
+              <pre>{ JSON.stringify(formErrors) }</pre>
+              <form onSubmit={handleSubmit} noValidate>
+                  <div>
+                      <input id="name" typ="text" placeholder="Your Name" value={contactForm.name} onChange={handleChange} />
+                      <div className="errorMessage">{formErrors.name}</div>
+                  </div>
+                  <div>
+                      <input id="email" typ="email" placeholder="Your Mail" value={contactForm.email} onChange={handleChange} />
+                      <div className="errorMessage">{formErrors.email}</div>
+                  </div>
+                  <div className="textarea">
+                      <textarea id="comment" placeholder="Comments" value={contactForm.comment} onChange={handleChange}></textarea>
+                      <div className="errorMessage">{formErrors.comment}</div>
+                  </div>
+                  <div className="formBtn">
+                    <button type="submit" className="btn-theme">Post Comments</button>
+                  </div>
+              </form>
+              </>
+            )
+          }
         </div>
     </section>
   )
